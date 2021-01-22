7e9a84390563f03e31cb0b378261ad9876ae6f86
ThermosDev <63248985+ThermosDev@users.noreply.github.com>
2021-01-22 18:04:32 +0100

    Structure  du projet
    Ajout de bootstrap, création des routes, du model de livre, header créé également

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,20 @@ import { BookListComponent } from './book-list/book-list.component';
 import { SingleBookComponent } from './book-list/single-book/single-book.component';
 import { BookFormComponent } from './book-list/book-form/book-form.component';
 import { HeaderComponent } from './header/header.component';
+import { AuthService } from './services/auth.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { BooksService } from './services/books.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'auth/signup', component: SignupComponent},
+  { path: 'auth/signin', component: SigninComponent},
+  { path: 'books', component: BookListComponent},
+  { path: 'books/new', component: BookFormComponent},
+  { path: 'books/view/:id', component: SingleBookComponent},
+];
 
 @NgModule({
   declarations: [
@@ -20,9 +34,17 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuardService,
+    BooksService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

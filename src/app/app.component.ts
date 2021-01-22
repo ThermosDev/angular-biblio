457024196fdb6b0079d592d19337eef0da3d8d57
ThermosDev <63248985+ThermosDev@users.noreply.github.com>
2021-01-22 18:19:47 +0100

    Intégration Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projet-biblio';
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyBcEKSi5JO0Tea7nbK-1aDCgrI2VVeXxjM",
+      authDomain: "projet-biblio-47a84.firebaseapp.com",
+      databaseURL: "https://projet-biblio-47a84-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "projet-biblio-47a84",
+      storageBucket: "projet-biblio-47a84.appspot.com",
+      messagingSenderId: "1076309675005",
+    };
+    firebase.initializeApp(config);
+  }
 }

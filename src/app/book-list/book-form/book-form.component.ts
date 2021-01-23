b1bc0b9a7c53f9f4c3a7ad86c10961f45c720a9d
ThermosDev <63248985+ThermosDev@users.noreply.github.com>
2021-01-23 15:14:45 +0100

    Storage
    Stockage de photo au sein de l'application

--- src/app/book-list/book-form/book-form.component.ts
+++ src/app/book-list/book-form/book-form.component.ts
@@ -12,6 +12,9 @@ import { Router } from '@angular/router';
 export class BookFormComponent implements OnInit {
 
   bookForm: FormGroup;
+  fileIsUploading = false;
+  fileUrl: string;
+  fileUploaded = false;
 
   constructor(private formBuilder: FormBuilder, private booksService: BooksService,
               private router: Router) { }
@@ -24,7 +27,8 @@ export class BookFormComponent implements OnInit {
     this.bookForm = this.formBuilder.group({
       title: ['', Validators.required],
       author: ['', Validators.required],
-      synopsis: ''
+      synopsis: '',
+      photo: ''
     });
   }
   
@@ -34,8 +38,25 @@ export class BookFormComponent implements OnInit {
     const synopsis = this.bookForm.get('synopsis').value;
     const newBook = new Book(title, author);
     newBook.synopsis = synopsis;
+    if(this.fileUrl && this.fileUrl !== '') {
+      newBook.photo = this.fileUrl;
+    }
     this.booksService.createNewBook(newBook);
     this.router.navigate(['/books']);
+  }
+
+  onUploadFile(file: File) {
+    this.fileIsUploading = true;
+    this.booksService.uploadFile(file).then(
+      (url: string) => {
+        this.fileUrl = url;
+        this.fileIsUploading = false;
+        this.fileUploaded = true;
+      }
+    );
+  }
+  detectFiles(event) {
+  this.onUploadFile(event.target.files[0]);
   }
 }
 

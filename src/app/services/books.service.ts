039b293758b595837f050337cfd9ef99bb117517
ThermosDev <63248985+ThermosDev@users.noreply.github.com>
2021-01-23 14:50:58 +0100

    BDD
    Création des méthodes pour le CRUD et création des vues et actions liées

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from '@angular/core';
+import firebase from 'firebase';
+import { Subject } from 'rxjs/Subject';
+import { Book } from '../models/book.model';
+import DataSnapshot = firebase.database.DataSnapshot;
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class BooksService {
 
-  constructor() { }
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
+  constructor() {
+    this.getBooks();
+   }
+
+  emitBooks(){
+    this.booksSubject.next(this.books);
+  }
+  //Enregistre la liste sur un node de la BDD
+  saveBooks(){
+    firebase.database().ref('/books').set(this.books);
+  }
+  //récupère la liste entière des livres
+  getBooks() {
+    firebase.database().ref('/books')
+      .on('value', (data: DataSnapshot) => {
+          this.books = data.val() ? data.val() : [];
+          this.emitBooks();
+        }
+      );
+  }
+  //récupère un seul livre
+  getSingleBook(id: number) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.database().ref('/books/' + id).once('value').then(
+          (data: DataSnapshot) => {
+            resolve(data.val());
+          }, (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+  //Création d'un livre
+  createNewBook(newBook: Book) {
+    this.books.push(newBook);
+    this.saveBooks();
+    this.emitBooks();
+  }
+  //Suppresion d'un livre
+  removeBook(book: Book) {
+    const bookIndexToRemove = this.books.findIndex(
+      (bookEl) => {
+        if(bookEl === book) {
+          return true;
+        }
+      }
+    );
+    this.books.splice(bookIndexToRemove, 1);
+    this.saveBooks();
+    this.emitBooks();
+  }
 }
